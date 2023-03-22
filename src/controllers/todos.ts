2873e49dd66956a01577e07489a61339d29785c8
sumito <sumito.temma@gmail.com>
2023-03-23 02:44:23 +0900

    first commit

---
+++ src/controllers/todos.ts
@@ -0,0 +1,54 @@
+
+import {RequestHandler} from 'express';
+import {Todo} from '../models/todo';
+
+const TODOS: Todo[] = [];
+
+export const createTodo: RequestHandler = (req,res,next) => {
+    const text = (req.body as {text: string}).text;
+    const newTodo = new Todo(Math.random().toString(),text);
+
+    TODOS.push(newTodo);
+
+    res.status(201).json({message: 'TODOを作成しました',createTodo:newTodo});
+
+}
+
+export const getTodos: RequestHandler = (req,res,next) => {
+
+    res.status(201).json({todos: TODOS});
+
+}
+
+
+export const updateTodo: RequestHandler<{id: string}> = (req,res,next) => {
+    const todoId = req.params.id;
+    const updateText = (req.body as {text:string}).text;
+
+    const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+
+    if( todoIndex <= 0 ){
+        throw new Error("対象のTODOが見つかりません");
+    }
+
+    TODOS[todoIndex] = new Todo(todoId,updateText);
+
+    res.json({message: "更新しました",updatedTodo: TODOS[todoIndex]});
+}
+
+
+
+export const deleteTodo: RequestHandler<{id: string}>  = (req,res,next) => {
+    const todoId = req.params.id;
+
+    const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+
+    if( todoIndex <= 0 ){
+        throw new Error("対象のTODOが見つかりません");
+    }
+
+    TODOS.splice(todoIndex,1);
+
+    res.json( { message: "TODOを削除しました。"});
+
+}
---
+++ src/routes/todos.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+
+import {createTodo} from '../controllers//todos';
+import {getTodos} from '../controllers//todos';
+import {updateTodo} from '../controllers//todos';
+import {deleteTodo} from '../controllers//todos';
+
+const router = Router();
+
+router.post('/',createTodo);
+
+router.get('/',getTodos);
+
+router.patch('/:id',updateTodo);
+
+router.delete('/:id',deleteTodo);
+
+
+export default router;
